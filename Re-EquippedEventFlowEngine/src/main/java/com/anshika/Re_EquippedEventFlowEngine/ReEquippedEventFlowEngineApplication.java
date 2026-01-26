1bf23602c65f81598d18e6e6021662886875ceeb
pihhuusrivastavv <pihusrivastav97@gmail.com>
2026-01-26 13:45:11 +0530

    Spring Boot setup complete with TestController

---
+++ Re-EquippedEventFlowEngine/src/main/java/com/anshika/Re_EquippedEventFlowEngine/ReEquippedEventFlowEngineApplication.java
@@ -0,0 +1,13 @@
+package com.anshika.Re_EquippedEventFlowEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReEquippedEventFlowEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReEquippedEventFlowEngineApplication.class, args);
+	}
+
+}
---
+++ Re-EquippedEventFlowEngine/src/test/java/com/anshika/Re_EquippedEventFlowEngine/ReEquippedEventFlowEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshika.Re_EquippedEventFlowEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReEquippedEventFlowEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
