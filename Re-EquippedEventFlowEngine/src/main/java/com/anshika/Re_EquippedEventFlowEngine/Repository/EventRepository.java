708430ced1feee5f01bf86f80f2f81509781dd2f
pihhuusrivastavv <pihusrivastav97@gmail.com>
2026-03-05 21:00:10 +0530

    Added EventRepository class for the events to be for creation of eentities, and hibernate orm.

--- Re-EquippedEventFlowEngine/src/main/java/com/anshika/Re_EquippedEventFlowEngine/Repository/EventRepository.java
+++ Re-EquippedEventFlowEngine/src/main/java/com/anshika/Re_EquippedEventFlowEngine/Repository/EventRepository.java
@@ -1,4 +1,11 @@
 package com.anshika.Re_EquippedEventFlowEngine.Repository;
 
-public class EventRepository {
+import com.anshika.Re_EquippedEventFlowEngine.ConstructorInitialization.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Integer>
+{
+
 }
